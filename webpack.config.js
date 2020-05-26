e9bded7141445b0b38652ff384bac4dba3abc18f
Laetitia B <bourgois.laetitia@gmail.com>
2020-05-26 16:05:10 +0200

    feat: Add http plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "production",
   entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Recipes App",
+      meta: { viewport: "width=device-width, initial-scale=1.0" },
+    }),
+  ],
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
