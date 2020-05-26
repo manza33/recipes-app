4cd98263ba6368dcfaec63e82cd2a5277ad790d4
Laetitia B <bourgois.laetitia@gmail.com>
2020-05-26 17:08:03 +0200

    Add htmlTemplate

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,28 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const htmlWebpackTemplate = require("html-webpack-template");
 
 module.exports = {
   mode: "production",
   entry: "./src/index.js",
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Recipes App",
-      meta: { viewport: "width=device-width, initial-scale=1.0" },
-    }),
-  ],
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Recipes App",
+      meta: { viewport: "width=device-width, initial-scale=1.0" },
+      inject: true,
+      template: htmlWebpackTemplate,
+      links: [
+        "https://fonts.googleapis.com/css2?family=Roboto:wght@100;400&display=swap",
+      ],
+    }),
+  ],
+
   module: {
     rules: [
       {
